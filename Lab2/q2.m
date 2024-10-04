e94628445a1ec64a7a37c5cf6bfdf5a1bec829d9
filthytv <filthytv@ludd.ltu.se>
2024-10-04 04:14:40 +0200

    4.a-d

--- Lab2/q2.m
+++ Lab2/q2.m
@@ -2,7 +2,7 @@ num_samples = 10000;
 uniform_samples = rand(num_samples, 12);  
 gaussian_samples = sum(uniform_samples, 2) - 6;  
 
-bin_width = 0.1;
+bin_width = 0.1; % 20/0.1 = 200 (=) 200 bins
 bin_edges = -10:bin_width:10; 
 histogram(gaussian_samples, 'BinEdges', bin_edges, 'Normalization', 'pdf'); 
 
@@ -11,7 +11,7 @@ hold on;
 x_values = -10:0.1:10; 
 mu = 0;  
 sigma = 1;  
-theoretical_gaussian = (1/(sigma * sqrt(2 * pi))) * exp(-(x_values - mu).^2 / (2 * sigma^2));
+theoretical_gaussian = (1/(sigma * sqrt(2*pi))) * exp(-((x_values - mu).^2) / (2 * sigma^2));
 
 plot(x_values, theoretical_gaussian, 'LineWidth', 2);  
 
--- Lab2/q3.m
+++ Lab2/q3.m
@@ -12,6 +12,7 @@ ylabel('f_X(x)');
 %b)
 N_values = [1000, 10000, 20000]; 
 bins = [20, 50, 100]; 
+n = 0;
 
 for N = N_values
     X = mu + sigma * randn(N, 1); 
@@ -21,5 +22,7 @@ for N = N_values
         histogram(X, b, 'Normalization', 'pdf');
         xlabel('x');
         ylabel('f_X(x)');
+        %saveas(gcf,num2str(n),'png')
+        n = n+1;
     end
 end
