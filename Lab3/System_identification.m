afd77b4e95da2ac2b6efeb219af503c2f754cdd9
Jim Agnevik <warpedrooster@gmail.com>
2024-10-28 10:23:25 +0100

    Finish system identification

--- Lab3/System_identification.m
+++ Lab3/System_identification.m
@@ -4,20 +4,51 @@ x = input_output.x;
 y = input_output.y;
 
 lag = 50;
+lag_plot = -lag:lag;
+
+max_M = 20;
 
 Rxx = xcorr(x, lag);
 Ryy = xcorr(y, lag);
 Rxy = xcorr(x, y, lag);
 
+figure(1)
+stem(lag_plot,Rxx)
+title("Auto-correlation Rxx[k]")
+figure(2)
+stem(lag_plot,Ryy)
+title("Auto-correlation Ryy[k]")
+figure(3)
+stem(lag_plot,Rxy)
+title("Cross-correlation Rxy[k]")
+
 fftRxx = fft(Rxx);
 fftRyy = fft(Ryy);
 fftRxy = fft(Rxy);
 
-x_plot = 1:length(fftRxx);
+figure(4)
+stem(lag_plot,abs(fftRxx))
+title("Power spectral density Rxx[k]")
+figure(5)
+stem(lag_plot,abs(fftRyy))
+title("Power spectral density Ryy[k]")
 
-figure(1)
-plot(x_plot,fftRxx)
-figure(2)
-plot(x_plot,fftRyy)
-figure(3)
-plot(x_plot,fftRxy)
+Ryx = xcorr(y, x, lag);
+ 
+MSE = zeros(1, max_M-1);
+
+for M = 2:max_M
+    Rxx_m = toeplitz(Rxx(lag+1:lag+1+M)); % As the function in matlab gives both sides we need to skip to lag 50 to be at 1
+    Ryx_v = Ryx(lag+1:lag+M+1);
+
+    h_est = Rxx_m \ Ryx_v;
+
+    y_est = filter(h_est, 1, x)
+    
+    MSE(M-1) = immse(y_est, y)
+end 
+
+plot_M = 2:max_M
+figure(6)
+plot(plot_M,MSE)
+title('M vs MSE')
